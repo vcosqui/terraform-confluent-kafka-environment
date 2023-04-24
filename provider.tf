fa246bb154b1cd3b9989966ecaf7435747739f79
vcosqui <victor.cosqui@gmail.com>
2023-04-24 08:37:30 +0200

    remove provider for compatibility with new tf modules

--- provider.tf
+++ provider.tf
@@ -8,8 +8,3 @@ terraform {
   }
   required_version = ">= 1.3.0"
 }
-
-provider "confluent" {
-  cloud_api_key    = var.confluent_cloud_api_key
-  cloud_api_secret = var.confluent_cloud_api_secret
-}
